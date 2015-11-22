6dd148ee798b8d7ca86c6d449d5bd92b6b99990b
Andre <inf1830@fh-worms.de>
2015-11-21 17:54:29 -0800

    added spotify player

--- app/src/main/java/edu/csumb/brainwavemusicfinder/MainActivity.java
+++ app/src/main/java/edu/csumb/brainwavemusicfinder/MainActivity.java
@@ -1,52 +1,220 @@
 package edu.csumb.brainwavemusicfinder;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
+import com.spotify.sdk.android.player.Config;
+import com.spotify.sdk.android.player.ConnectionStateCallback;
+import com.spotify.sdk.android.player.Player;
+import com.spotify.sdk.android.player.PlayerNotificationCallback;
+import com.spotify.sdk.android.player.PlayerState;
+import com.spotify.sdk.android.player.Spotify;
+
+import org.json.JSONObject;
+
+public class MainActivity extends Activity implements
+        PlayerNotificationCallback, ConnectionStateCallback {
+
+    // TODO: Replace with your client ID
+    private static final String CLIENT_ID = "4976a9e391f74baebbc1d44e515b6a2e";
+    // TODO: Replace with your redirect URI
+    private static final String REDIRECT_URI = "spotifytester://callback";
+
+    private Player mPlayer;
+    private IOSocket socket;
+    private String mood;
+    private int someVariable = 0;
+    private TextView moodState;
+    private ImageView img;
+    // Request code that will be used to verify if the result comes from correct activity
+// Can be any integer
+    private static final int REQUEST_CODE = 1337;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        connect();
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        img = (ImageView) findViewById(R.id.imageView);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(CLIENT_ID,
+                AuthenticationResponse.Type.TOKEN,
+                REDIRECT_URI);
+        builder.setScopes(new String[]{"user-read-private", "streaming"});
+        AuthenticationRequest request = builder.build();
+
+        AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
+
+        FloatingActionButton detectButton = (FloatingActionButton) findViewById(R.id.detect);
+        detectButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                moodState = (TextView) findViewById(R.id.textView);
+                moodState.setText(mood);
+                if (mood.equals("meditation")) {
+                    img.setImageResource(R.drawable.meditation);
+                } else if (mood.equals("happy")) {
+                    img.setImageResource(R.drawable.happy);
+                }
+            }
+        });
+
+
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        if (requestCode == REQUEST_CODE) {
+            AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
+            if (response.getType() == AuthenticationResponse.Type.TOKEN) {
+                final Config playerConfig = new Config(this, response.getAccessToken(), CLIENT_ID);
+                // Check if result comes from the correct activity
+                Spotify.getPlayer(playerConfig, this, new Player.InitializationObserver() {
+                    @Override
+                    public void onInitialized(Player player) {
+                        mPlayer = player;
+                        mPlayer.addConnectionStateCallback(MainActivity.this);
+                        mPlayer.addPlayerNotificationCallback(MainActivity.this);
+                        mPlayer.setShuffle(true);
+                        mPlayer.play("spotify:user:spotify:playlist:1oXl0OHlE1mPDChMa8Y0Ax");
+                    }
+
+                    @Override
+                    public void onError(Throwable throwable) {
+                        Log.e("MainActivity", "Could not initialize player: " + throwable.getMessage());
+                    }
+                });
+            }
+        };
+        Button happyButton = (Button) findViewById(R.id.happyButton);
+        happyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mPlayer.setShuffle(true);
+                mPlayer.play("spotify:user:spotify:playlist:1B9o7mER9kfxbmsRH9ko4z");
+                mood = "happy";
+            }
+        });
+
+
+        Button nextButton = (Button) findViewById(R.id.nextButton);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mPlayer.skipToNext();
+            }
+        });
+        Button pauseButton = (Button) findViewById(R.id.pauseButton);
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mPlayer.pause();
+            }
+        });
+        Button mediButton = (Button) findViewById(R.id.mediButton);
+        mediButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mPlayer.setShuffle(true);
+                mPlayer.play("spotify:user:spotify:playlist:1oXl0OHlE1mPDChMa8Y0Ax");
+                mood = "meditation";
             }
         });
     }
 
+
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void onLoggedIn() {
+        Log.d("MainActivity", "User logged in");
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void onLoggedOut() {
+        Log.d("MainActivity", "User logged out");
+    }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+    @Override
+    public void onLoginFailed(Throwable error) {
+        Log.d("MainActivity", "Login failed");
+    }
 
-        return super.onOptionsItemSelected(item);
+    @Override
+    public void onTemporaryError() {
+        Log.d("MainActivity", "Temporary error occurred");
+    }
+
+    @Override
+    public void onConnectionMessage(String message) {
+        Log.d("MainActivity", "Received connection message: " + message);
+    }
+
+    @Override
+    public void onPlaybackEvent(EventType eventType, PlayerState playerState) {
+        Log.d("MainActivity", "Playback event received: " + eventType.name());
+    }
+
+    @Override
+    public void onPlaybackError(ErrorType errorType, String errorDetails) {
+        Log.d("MainActivity", "Playback error received: " + errorType.name());
+    }
+
+    @Override
+    protected void onDestroy() {
+        Spotify.destroyPlayer(this);
+        super.onDestroy();
+    }
+
+    void connect() {
+
+        socket = new IOSocket("http://aipservers.com:3001", new MessageCallback() {
+
+            @Override
+            public void onMessage(String message) {
+                // Handle simple messages
+            }
+
+            @Override
+            public void onConnect() {
+                // Socket connection opened
+            }
+
+            @Override
+            public void onDisconnect() {
+                // Socket connection closed
+                Log.d("Disconnect", "Disconnect");
+            }
+
+            @Override
+            public void on(String event, JSONObject... data) {
+                Log.d("test123",event);
+                mood = event;
+            }
+
+            @Override
+            public void onMessage(JSONObject json) {
+                Log.d("test123","json");
+            }
+
+            @Override
+            public void onConnectFailure() {
+
+            }
+        });
+
+        socket.connect();
     }
 }
